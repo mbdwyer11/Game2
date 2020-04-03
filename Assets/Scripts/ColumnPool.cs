1a482f5bb4b8663765ec8045247368f49daca160
mbdwyer11 <62067690+mbdwyer11@users.noreply.github.com>
2020-04-03 14:48:16 -0400

    Add files via upload

---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+    public int ColumnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 8f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15, -25);
+    private float timeSinceLastSpawned;
+    private float spawnXPosition = 5f;
+    private int currentColumn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        columns = new GameObject[ColumnPoolSize];
+        for (int i = 0; i < ColumnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= ColumnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
